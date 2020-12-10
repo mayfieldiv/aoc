3a43e5971b1560d92b08056b5a8ad09346934ec9
mayfield <3902405+mayfieldiv@users.noreply.github.com>
2020-12-10 00:51:23 -0500

    10

--- 2020/10.py
+++ 2020/10.py
@@ -1,19 +1,41 @@
-# 10.in
-import sys, re, math, collections, functools
-# from collections import defaultdict, Counter
-# from functools import reduce, lru_cache
+import sys
+from utils import clip
 
-# groups = [x.strip().split('\n') for x in sys.stdin.read().split("\n\n")]
-# lines = [x.strip() for x in sys.stdin]
-# nums = list(map(int, input().split()))
+w = [int(x) for x in sys.stdin]
+w += [0, max(w) + 3]
+w.sort()
 
 def solve():
-
-    return
+    print(w)
+    c1 = c3 = 0
+    for a, b in zip(w, w[1:]):
+        if b-a == 1: c1 += 1
+        elif b-a == 3: c3 += 1
+        else: assert False
+    print(c1, c3)
+    return c1 * c3
 
 def solve2():
-
     return
+    start = 0
+    count = 1
+    for i, (a, b) in enumerate(zip(w, w[1:])):
+        if b-a == 3:
+            if (i - start > 1):
+                print(w[start:i+1])
+                opt = i - start - 1
+                assert opt <= 3
+                if opt == 3: count *= 7
+                else: count *= 2**opt
+            start = i+1
+    return count
 
-print(solve())
-print(solve2())
+print("******************* PART 1 *******************")
+s1 = solve()
+print("solve():", s1)
+
+s2 = solve2()
+print("\n******************* PART 2 *******************")
+print("solve2():", s2)
+
+clip(s2 if s2 != None else s1)
