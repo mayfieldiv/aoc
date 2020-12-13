55fd3ba5d72f0739f2a393d11ffe11ee7f157704
mayfield <3902405+mayfieldiv@users.noreply.github.com>
2020-12-13 00:54:27 -0500

    clean up

--- 2020/13.py
+++ 2020/13.py
@@ -1,50 +1,35 @@
-# 13.in.txt
-import sys, re, math, collections, functools
+import math
 from utils import clip
-# from collections import defaultdict, Counter
-# from functools import reduce, lru_cache
-
-# lines = [x.strip().split('\n') for x in sys.stdin.read().split("\n\n")]
-# lines = [x.strip() for x in sys.stdin]
-# lines = [int(x) for x in sys.stdin]
-# lines = list(map(int, input().split()))
 
 start = int(input())
 ids = input().split(',')
 
 def solve():
     valid = [int(x) for x in ids if x != 'x']
-    wait = 999999
-    best = 0
+    best = (valid[0], valid[0])
     for v in valid:
-        m = v - (start % v)
-        if m < wait:
-            wait = m
-            best = v
-        print(m, v, wait, best)
-    return wait * best
+        m = -start % v
+        if m < best[1]:
+            best = v, m
+        print(v, m, best)
+    return math.prod(best)
 
-def mul_inv(a, b):
-    b0 = b
-    x0, x1 = 0, 1
-    if b == 1: return 1
-    while a > 1:
-        q = a // b
-        a, b = b, a%b
-        x0, x1 = x1 - q * x0, x0
-    if x1 < 0: x1 += b0
-    return x1
- 
+def mul_inv(a, mod):
+    for x in range(1, mod):
+        if (a * x) % mod == 1:
+            return x
 
 def solve2():
-    valid = [(int(x), int(x)-i) for i, x in enumerate(ids) if x != 'x']
+    valid = [(int(x), -i) for i, x in enumerate(ids) if x != 'x']
     print(valid)
+    # chinese remainder
     prod = math.prod([x[0] for x in valid])
     sum = 0
-    for n_i, a_i in valid:
-        p = prod / n_i
-        sum += a_i * mul_inv(p, n_i) * p
-    return int(sum % prod)
+    for n, a in valid:
+        p = prod // n
+        sum += a * mul_inv(p, n) * p
+    print(sum)
+    return sum % prod
 
 print("******************* PART 1 *******************")
 s1 = solve()
