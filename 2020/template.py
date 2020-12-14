1023a6d17940b0c61f5693395f1b4ba791614296
mayfield <3902405+mayfieldiv@users.noreply.github.com>
2020-12-14 01:44:16 -0500

    template

--- 2020/template.py
+++ 2020/template.py
@@ -1,15 +1,15 @@
 # xx.in.txt
-import sys, re, math, collections, functools
-from utils import clip
-# from collections import defaultdict, Counter
+import sys, re, math, itertools, heapq
+from utils import clip, main
+# from collections import Counter, defaultdict, deque, OrderedDict
 # from functools import reduce, lru_cache
 
-# lines = [x.strip().split('\n') for x in sys.stdin.read().split("\n\n")]
 # lines = [x.strip() for x in sys.stdin]
 # lines = [int(x) for x in sys.stdin]
 # lines = list(map(int, input().split()))
+# lines = [x.strip().split('\n') for x in sys.stdin.read().split("\n\n")]
 
-def solve():
+def solve1():
     
     return
 
@@ -17,13 +17,4 @@ def solve2():
     
     return
 
-print("******************* PART 1 *******************")
-s1 = solve()
-print("solve():", s1)
-
-print("\n******************* PART 2 *******************")
-s2 = solve2()
-print("solve2():", s2)
-
-if s2 != None: clip(s2)
-elif s1 != None: clip(s1)
+main(solve1, solve2)
--- 2020/utils.py
+++ 2020/utils.py
@@ -5,3 +5,20 @@ def clip(obj):
     cmd = 'printf ' + s + ' | xsel --clipboard'
     subprocess.check_call(cmd, shell=True)
     print('Copied "' + s + '" to clipboard')
+
+def main(solve1, solve2):
+    print("******************* PART 1 *******************")
+    s1 = solve1()
+    print("solve1():", s1)
+
+    print("\n******************* PART 2 *******************")
+    s2 = solve2()
+    print("solve2():", s2)
+
+    if s2 != None:
+        clip(s2)
+        return s2
+    elif s1 != None:
+        clip(s1)
+        return s1
+
