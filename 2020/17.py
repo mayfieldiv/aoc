8af5c65fda457804160cf9b8dcf4f68b05d718c8
mayfield <3902405+mayfieldiv@users.noreply.github.com>
2020-12-17 02:57:33 -0500

    general solution but SLLLLOOOOOOWWWW. need numpy

--- 2020/17.py
+++ 2020/17.py
@@ -1,4 +1,4 @@
-import sys
+import sys, itertools
 from utils import clip, main
 from collections import Counter
 
@@ -26,88 +26,59 @@ def mat_apply(mat, func):
 
 def mat_shape(mat):
     shape = []
-    while isinstance(mat, list):
-        shape.append(len(mat))
-        if len(mat) == 0:
+    val = mat
+    while isinstance(val, list):
+        shape.append(len(val))
+        if len(val) == 0:
             break
-        mat = mat[0]
+        val = val[0]
     return shape
 
-def mat_sum(mat, selector = None):
+def mat_sum(mat, selector=None):
     return sum((selector(val) if selector != None else val) for val, *_ in mat_iterate(mat))
 
-# def mat_neighbors(mat):
+def mat_index(mat, index):
+    res = mat
+    for i in index:
+        res = res[i]
+    return res
 
+def mat_set_index(mat, index, val):
+    res = mat
+    for i in index[:-1]:
+        res = res[i]
+    res[index[-1]] = val
 
-def a(v, *i):
-    print(i, v)
-    return v + '.'
+def mat_create(shape, val):
+    return [mat_create(shape[1:], val) if len(shape) > 1 else val for _ in range(shape[0])]
 
+def mat_neighbors(mat, index):
+    shape = mat_shape(mat)
+    dims = len(shape)
+    for delta in itertools.product((-1, 0, 1), repeat=dims):
+        if delta != tuple([0]*dims) and all(0 <= index[i]+delta[i] < shape[i] for i in range(dims)):
+            neigh_index = [x+d for x,d in zip(index, delta)]
+            yield [mat_index(mat, neigh_index), *neigh_index]
 
-for v, *i in mat_iterate([lines]):
-    print(i, v)
+def expand(mat):
+    shape = mat_shape(mat)
+    new = mat_create([x+2 for x in shape], '.')
+    mat_apply(mat, lambda val,*index: mat_set_index(new, [x+1 for x in index], val))
+    return new
 
-def expand3(base):
-    shape = mat_shape(base)
-    grid = [[['.'
-        for k in range(shape[2]+2)]
-        for j in range(shape[1]+2)]
-        for i in range(shape[0]+2)]
-    def update(val,i,j,k): grid[i+1][j+1][k+1] = val
-    mat_apply(base, update)
-    return grid
-
-def expand4(base):
-    grid = [[[['.'
-        for l in range(shape[3]+2)]
-        for k in range(shape[2]+2)]
-        for j in range(shape[1]+2)]
-        for i in range(shape[0]+2)]
-    def update(val,i,j,k,l): grid[i+1][j+1][k+1][l+1] = val
-    mat_apply(base, update)
-    return grid
-
-def solve1():
-    grid = expand3([lines])
+def solve():
+    global lines
+    lines = [lines]
+    grid = expand(lines)
     for step in range(6):
         shape = mat_shape(grid)
-        print(step, shape)
-        def update(val,i,j,k):
-            n = sum(grid[i+di][j+dj][k+dk] == '#'
-                for di in (-1,0,1)
-                for dj in (-1,0,1)
-                for dk in (-1,0,1)
-                if (di,dj,dk) != (0,0,0)
-                and 0 <= i+di < shape[0]
-                and 0 <= j+dj < shape[1]
-                and 0 <= k+dk < shape[2]
-            )
+        print(step, shape, mat_sum(grid, lambda x: x == '#'))
+        def update(val,*index):
+            n = sum(neighbor == '#' for neighbor, *_ in mat_neighbors(grid, index))
             return '#' if n == 3 or (val == '#' and n == 2) else '.'
 
-        grid = expand3(mat_apply(grid, update))
+        grid = expand(mat_apply(grid, update))
 
     return mat_sum(grid, lambda x: x == '#')
 
-def solve2():
-    grid = expand4([[lines]])
-    for step in range(6):
-        print(step)
-        def update(val,i,j,k,l):
-            n = sum(grid[i+di][j+dj][k+dk][l+dl] == '#'
-                for di in (-1,0,1)
-                for dj in (-1,0,1)
-                for dk in (-1,0,1)
-                for dl in (-1,0,1)
-                if (di,dj,dk,dl) != (0,0,0,0)
-                and 0 <= i+di < shape[0]
-                and 0 <= j+dj < shape[1]
-                and 0 <= k+dk < shape[2]
-                and 0 <= l+dl < shape[3]
-            )
-            return '#' if n == 3 or (val == '#' and n == 2) else '.'
-
-        grid = expand4(mat_apply(grid, update))
-
-    return mat_sum(grid, lambda x: x == '#')
-
-main(solve1, solve2)
+main(solve, solve)
