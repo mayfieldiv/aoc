66a92eddb82aba6f89681e895f1c6cbc4be576b2
Mayfield <git@mayfieldiv.com>
2024-12-07 09:49:44 -0500

    5b
    5b

--- 2024/src/bin/5.rs
+++ 2024/src/bin/5.rs
@@ -1,4 +1,7 @@
-use std::collections::{HashMap, HashSet};
+use std::{
+	cmp::Ordering,
+	collections::{HashMap, HashSet},
+};
 
 fn main() -> anyhow::Result<()> {
 	let mut input = String::new();
@@ -8,10 +11,11 @@ fn main() -> anyhow::Result<()> {
 	for (before, after) in raw_rules.lines().map(|line| line.split_once('|').unwrap()) {
 		rules.entry(before.parse()?).or_default().insert(after.parse()?);
 	}
+	// println!("{:#?}", rules);
 
 	let mut result = 0u64;
 	for update in updates.lines() {
-		let pages: Vec<u8> = update.split(',').map(|s| s.parse().unwrap()).collect();
+		let mut pages: Vec<u8> = update.split(',').map(|s| s.parse().unwrap()).collect();
 		fn check(rules: &HashMap<u8, HashSet<u8>>, pages: &[u8]) -> bool {
 			for (i, &page) in pages.iter().enumerate() {
 				for later_idx in i + 1..pages.len() {
@@ -25,9 +29,27 @@ fn main() -> anyhow::Result<()> {
 			}
 			true
 		}
-		if check(&rules, &pages) {
-			let middle = pages[pages.len() / 2];
-			result += middle as u64;
+		if cfg!(feature = "part1") {
+			if check(&rules, &pages) {
+				let middle = pages[pages.len() / 2];
+				result += middle as u64;
+			}
+		} else {
+			if !check(&rules, &pages) {
+				// println!("BEFORE {:?}", pages);
+				pages.sort_by(|a, b| {
+					if rules.get(b).map(|rules| rules.contains(a)).unwrap_or(false) {
+						Ordering::Less
+					} else if rules.get(a).map(|rules| rules.contains(b)).unwrap_or(false) {
+						Ordering::Greater
+					} else {
+						Ordering::Equal
+					}
+				});
+				// println!("AFTER {:?}", pages);
+				let middle = pages[pages.len() / 2];
+				result += middle as u64;
+			}
 		}
 	}
 
